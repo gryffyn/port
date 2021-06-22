dbb233b8546783252cd52895a7e3ec088ee2a866
gryffyn <me@neveris.one>
2021-06-22 21:23:21 +0100

    added linter to ci
    Signed-off-by: gryffyn <me@neveris.one>

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.16
 require (
 	github.com/alexflint/go-arg v1.4.2
 	github.com/fatih/color v1.12.0
-	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/tools v0.1.3 // indirect
+	mvdan.cc/gofumpt v0.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-    "log"
-    "os/exec"
-    "strconv"
-    "strings"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
 
-    "github.com/alexflint/go-arg"
-    "github.com/fatih/color"
+	"github.com/alexflint/go-arg"
+	"github.com/fatih/color"
 )
 
 func main() {
-    var vals []string
+	var vals []string
 
-    var args struct {
-        Port    int   `arg:"positional,required"`
-        Simple  bool  `arg:"-s,--simple" help:"Show simplified output (just the protocol, local address, and PID/process"`
-    }
-    arg.MustParse(&args)
+	var args struct {
+		Port   int  `arg:"positional,required"`
+		Simple bool `arg:"-s,--simple" help:"Show simplified output (just the protocol, local address, and PID/process"`
+	}
+	arg.MustParse(&args)
 
-    portInfo, err := exec.Command("/usr/bin/netstat", "-tulpn").Output()
-    if err != nil { log.Fatalln(err) }
-    lines := strings.Split(string(portInfo), "\n")
+	portInfo, err := exec.Command("/usr/bin/netstat", "-tulpn").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	lines := strings.Split(string(portInfo), "\n")
 
-    found := false
-    for _,v := range lines {
-        if strings.Contains(v, ":" + strconv.Itoa(args.Port)) {
-            found = true
-            split := strings.Split(v, " ")
-            for _,v2 := range split {
-                if strings.TrimSpace(v2) != "" {
-                    vals = append(vals, v2)
-                }
-            }
-        }
-    }
+	found := false
+	for _, v := range lines {
+		if strings.Contains(v, ":"+strconv.Itoa(args.Port)) {
+			found = true
+			split := strings.Split(v, " ")
+			for _, v2 := range split {
+				if strings.TrimSpace(v2) != "" {
+					vals = append(vals, v2)
+				}
+			}
+		}
+	}
 
-    if found {
-        printVals(vals, args.Simple)
-    }
+	if found {
+		printVals(vals, args.Simple)
+	}
 }
 
-
 func printVals(vals []string, simple bool) {
-    key := color.New(color.Bold, color.FgWhite).PrintfFunc()
-    value := color.New(color.FgCyan).PrintlnFunc()
-    keys := []string{"Protocol", "Recv-Q", "Send-Q", "Local Addr", "Remote Addr", "State", "PID/Process)"}
+	key := color.New(color.Bold, color.FgWhite).PrintfFunc()
+	value := color.New(color.FgCyan).PrintlnFunc()
+	keys := []string{"Protocol", "Recv-Q", "Send-Q", "Local Addr", "Remote Addr", "State", "PID/Process)"}
 
-    if simple {
-        simpleVals := []int{0,3,6}
-        for _,v := range simpleVals {
-            key("%-14v", keys[v] + ": ")
-            value(vals[v])
-        }
-
-    } else {
-        for k,v := range keys {
-            key("%-14v", v + ": ")
-            value(vals[k])
-        }
-    }
+	if simple {
+		simpleVals := []int{0, 3, 6}
+		for _, v := range simpleVals {
+			key("%-14v", keys[v]+": ")
+			value(vals[v])
+		}
+	} else {
+		for k, v := range keys {
+			key("%-14v", v+": ")
+			value(vals[k])
+		}
+	}
 }
